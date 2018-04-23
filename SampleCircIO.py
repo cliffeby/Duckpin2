2aa0289d3ef6fa0c90bfe40d1bee0a8f7281ab4d
cliffeby <clifford.eby@gmail.com>
2018-04-22 22:54:37 -0400

    chang timings

--- SampleCircIO.py
+++ SampleCircIO.py
@@ -4,7 +4,7 @@ import picamera
 
 def write_now():
     # Randomly return True (like a fake motion detection routine)
-    return random.randint(0, 5) == 0
+    return random.randint(0, 10) == 0
 
 def write_video(stream):
     global counter
@@ -12,15 +12,17 @@ def write_video(stream):
     with stream.lock:
         # Find the first header frame in the video
         for frame in stream.frames:
+            print('FRAME TYPE', frame.frame_type)
             if frame.frame_type == picamera.PiVideoFrameType.sps_header:
                 stream.seek(frame.position)
+                print("FRAME POSITION", frame.position)
                 break
         # Write the rest of the stream to disk
-        with io.open('../videos/motion'+ str(counter)+ '.h264', 'wb') as output:
+        with io.open('../videos/motion62sec'+ str(counter)+ '.h264', 'wb') as output:
             output.write(stream.read())
 counter = 0
 with picamera.PiCamera() as camera:
-    stream = picamera.PiCameraCircularIO(camera, seconds=3)
+    stream = picamera.PiCameraCircularIO(camera, seconds=6)
     camera.start_recording(stream, format='h264')
     try:
         while True:
@@ -31,5 +33,6 @@ with picamera.PiCamera() as camera:
                 camera.wait_recording(2)
                 counter +=1
                 write_video(stream)
+                stream.clear()
     finally:
         camera.stop_recording()
