51451c42afa60a51d42b41bd9934d030ad6361ff
cliffeby <clifford.eby@gmail.com>
2018-11-04 09:58:04 -0500

    update

--- threadTest.py
+++ threadTest.py
@@ -5,16 +5,25 @@ import random
 
 def trip():
     print('looking for ball')
-    while True:
-        if random.random() > .95:
-            print("Balllllllll")
-        time.sleep(0.1)
+    i = 0
+    try:
+        while True:
+            if random.random() > .90:
+                i = i+1
+                print("Balllllllll", i)
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        pass
 
 
 t = threading.Thread(target=trip, name='thread1')
 t.start()
 
 print('Statred')
-while True:
-    print('Still running')
-    time.sleep(5.0)
+try:
+    while True:
+        print('Still running')
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    t._stop()
+    pass
