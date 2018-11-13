91bd7308b7456f648b1752cf8e5f7eee1c7dec89
cliffeby <clifford.eby@gmail.com>
2018-11-13 16:35:19 -0500

    crops for 640

--- vidShowCrops640.py
+++ vidShowCrops640.py
@@ -3,6 +3,7 @@ import io
 import time
 import cv2
 import numpy as np
+import cropdata640
 # import picamera
 # from PIL import Image
 
@@ -13,10 +14,9 @@ y=0
 frameNo = 0
 
 # crop_ranges are y,y1,x,x1 from top left
-ball_crop_ranges = ([320,475,40,555],[0,0,0,0])
-reset_crop_ranges = ([155,270,540,600],[0,0,0,0])
-pin_crop_ranges = ([235,260, 315,340],[205,230, 290,315],[205,230,365,390],[180,205, 260,280],[180,205, 335,360],
-    [180,205, 410,435],[155,180, 250,275],[155,180, 315,335],[155,180, 380,400],[155,180, 450,470])
+ball_crop_ranges = cropdata640.ballCrops
+reset_crop_ranges = cropdata640.resetArmCrops
+pin_crop_ranges = cropdata640.pin_crop_ranges
 
 def drawPinRectangles():
     global pin_image
@@ -33,9 +33,10 @@ def drawPinRectangles():
             cv2.putText(pin_image,str(a),a,cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(255,255,255),2)
             cv2.putText(pin_image,str(b),b,cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(255,255,255),2)
     if frameNo==11:
-        cv2.imwrite('../videos/AAA/VidCombinedMask.jpg',pin_image)
+        cv2.imwrite('C:/Users/cliff/OneDrive/pyProjects/videos/AAA/VidCombinedMask2.jpg',pin_image)
     else:
-        cv2.imwrite('../videos/AAA/VidPinMask.jpg',pin_image)
+        print('frame11')
+        cv2.imwrite('C:/Users/cliff/OneDrive/pyProjects/videos/AAA/VidPinMask2.jpg',pin_image)
 
 def drawBallRectangles():
     global ball_image
@@ -55,7 +56,8 @@ def drawBallRectangles():
         if i == 0:
             cv2.putText(ball_image,str(a),a,cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
             cv2.putText(ball_image,str(b),(b[0]-250,b[1]),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2)
-    cv2.imwrite('../videos/AAA/AVidBallMask.jpg',ball_image)
+    cv2.imwrite('C:/Users/cliff/OneDrive/pyProjects/videos/AAA/VidBallMask2.jpg',ball_image)
+    print('frame')
 
 def detect_motion():
     global frameNo
@@ -74,20 +76,21 @@ def detect_motion():
     if frameNo == 10:
         ball_image = image
         drawBallRectangles()
-    if frameNo == 11:
-        ball_image = image
-        drawBallRectangles()
-        pin_image = ball_image
-        drawPinRectangles()
+    # if frameNo == 11:
+    #     ball_image = image
+    #     drawBallRectangles()
+    #     pin_image = ball_image
+    #     drawPinRectangles()
 
     return True
        
 # initialize the camera and grab a reference to the raw camera capture
-cap = cv2.VideoCapture('../videos/AAA/video640.h264')
+cap = cv2.VideoCapture('../vidim/All/video0.h264')
 while(cap.isOpened()):
     ret, frame2 = cap.read()
     if frameNo<15:
        detect_motion()
+       
     else:
         print("Complete.  Images in videos/AAA")
         break
