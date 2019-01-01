01bdd68237b1c56669211c5bc62fc8626a36b1e5
cliffeby <clifford.eby@gmail.com>
2019-01-01 17:30:10 -0500

    ribbon GPIO

--- myGPIO.py
+++ myGPIO.py
@@ -1,22 +1,23 @@
-# 7-segemnt 
-segment7s = [18, 24, 25, 7, 8, 23, 9, 4]
-segment7_0 = [18, 24, 25, 7, 8, 23]
-segment7_1 = [18, 24]
-segment7_2 = [18, 25, 7, 23, 9]
-segment7_3 = [18, 24, 25, 7, 9]
-segment7_4 = [18, 24, 8, 9]
-segment7_5 = [24, 25, 8, 7, 9]
-segment7_6 = [24, 25, 23, 8, 9]
-segment7_7 = [18, 24, 7]
-segment7_8 = [18, 24, 25, 7, 8, 23, 9]
-segment7_9 = [18, 24, 8, 7, 9]
-segment7All = [segment7_0, segment7_1, segment7_2, segment7_3, segment7_4, segment7_5, segment7_6, segment7_7, segment7_8, segment7_9]
+# 7-segemnt
+segment7s = [14, 15, 18, 23, 24, 25, 8, 7]
+segment7_0 = [14, 15, 18, 23, 24, 25]
+segment7_1 = [14, 15]
+segment7_2 = [14, 18, 23, 25, 8]
+segment7_3 = [14, 15, 18, 25, 8]
+segment7_4 = [14, 15, 24, 8]
+segment7_5 = [15, 18, 18, 25, 8]
+segment7_6 = [15, 18, 23, 25, 8]
+segment7_7 = [14, 15, 25]
+segment7_8 = [14, 15, 18, 23, 24, 25, 8]
+segment7_9 = [14, 15, 18, 24, 25]
+segment7All = [segment7_0, segment7_1, segment7_2, segment7_3,
+               segment7_4, segment7_5, segment7_6, segment7_7, segment7_8, segment7_9]
 # Pin leds
-pinsGPIO = [15,14,3,17,21,20,16,5,26,6]
+pinsGPIO = [5, 11, 9, 10, 22, 27, 17, 4, 3, 2]
 #  Laser trip-wire GPIO
 sensor = [12]
 
-nsegment7s = [14,15,18, 23, 24, 25, 8, 7]
-npinsGPIO = [5,10,9,11,17,27,22,2,3,4]
+nsegment7s = [14, 15, 18, 23, 24, 25, 8, 7]
+npinsGPIO = [5, 11, 9, 10, 22, 27, 17, 4, 3, 2]
 #  Laser trip-wire GPIO
 nsensor = [12]
