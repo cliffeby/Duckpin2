cd339484adc387f0f10d19cc15d959299869a7c7
cliffeby <clifford.eby@gmail.com>
2019-01-13 23:38:23 -0500

    rewire

--- myGPIO.py
+++ myGPIO.py
@@ -1,23 +1,18 @@
 # 7-segemnt
-segment7s = [14, 15, 18, 23, 24, 25, 8, 7]
-segment7_0 = [14, 15, 18, 23, 24, 25]
-segment7_1 = [14, 15]
-segment7_2 = [14, 18, 23, 25, 8]
-segment7_3 = [14, 15, 18, 25, 8]
-segment7_4 = [14, 15, 24, 8]
-segment7_5 = [15, 18, 18, 25, 8]
-segment7_6 = [15, 18, 23, 25, 8]
-segment7_7 = [14, 15, 25]
-segment7_8 = [14, 15, 18, 23, 24, 25, 8]
-segment7_9 = [14, 15, 18, 24, 25]
+segment7s = [8, 24, 23, 15, 7, 25, 14]
+segment7_0 = [8, 24, 23, 15, 7, 25]
+segment7_1 = [8, 24]
+segment7_2 = [8, 23, 15, 25, 14]
+segment7_3 = [8, 24, 23, 25, 14]
+segment7_4 = [8, 24, 7, 14]
+segment7_5 = [24, 23, 7, 25, 14]
+segment7_6 = [24, 23, 15, 7, 14]
+segment7_7 = [8, 24, 25]
+segment7_8 = [8, 24, 23, 15, 7, 25, 14]
+segment7_9 = [8, 24, 23, 7, 25, 14]
 segment7All = [segment7_0, segment7_1, segment7_2, segment7_3,
                segment7_4, segment7_5, segment7_6, segment7_7, segment7_8, segment7_9]
 # Pin leds
 pinsGPIO = [5, 11, 9, 10, 22, 27, 17, 4, 3, 2]
 #  Laser trip-wire GPIO
 sensor = [12]
-
-nsegment7s = [14, 15, 18, 23, 24, 25, 8, 7]
-npinsGPIO = [5, 11, 9, 10, 22, 27, 17, 4, 3, 2]
-#  Laser trip-wire GPIO
-nsensor = [12]
