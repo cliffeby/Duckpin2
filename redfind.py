8dac1dbbf49f205bbb8d8ffa6f89b14eda089282
cliffeby <clifford.eby@gamil.com>
2022-08-19 11:28:07 -0400

    check push

--- redfind.py
+++ redfind.py
@@ -4,6 +4,7 @@ import cv2
 image = cv2.imread('/home/cliffeby/Downloads/imgdpThursday (1).jpg')
 result = image.copy()
 image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+# Set using redslide on a 08/19/22 image
 lower = np.array([0,42,0])
 upper = np.array([16,255,126])
 mask = cv2.inRange(image, lower, upper)
